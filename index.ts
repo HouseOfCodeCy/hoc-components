264ec199a4c4a80652b6ee3955015a070aee5419
Marios Charalambous <mcharalambous@vubiquity.net>
2022-12-05 19:45:44 +0200

    adding eslint

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
-import Button from "./components/Button/Button";
+import Button from "./src/components/Button/Button";
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 export { Button };
